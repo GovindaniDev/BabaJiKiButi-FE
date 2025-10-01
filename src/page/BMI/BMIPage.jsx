2ae962466a53d483f8ca9ca4907d2e641c5870ca
AyushYadav1113 <ayushyadav212121@gmail.com>
2025-10-01 18:26:28 +0530

    File added

--- src/page/BMI/BMIPage.jsx
+++
@@ -1,8 +0,0 @@
-import BMICalculator from "../../components/BMICalculator";
-
-function BMIPage() {
-    window.scrollTo(0, 0);
-  return <BMICalculator />;
-}
-
-export default BMIPage;
--- src/page/panchkarma/PanchkarmaPage.jsx
+++
@@ -1,9 +0,0 @@
-import PanchkarmaSection from "../../components/PanchkarmaSection";
-
-
-function PanchkarmaPage() {
-    window.scrollTo(0, 0);
-  return <PanchkarmaSection />;
-}
-
-export default PanchkarmaPage;
